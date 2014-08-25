2d4d9b252b718267574cff71488ac49596653bbf
Andrew Chao <andrew@Andrews-MacBook-Pro-2.local>
2014-08-24 23:17:59 -0700

    Settings: Save to userdefaults
    Save the tip defaults to user defaults so they persist in memory
    Changes in the settings page should immediately reflect on the main view

--- TippingPot/SettingsViewController.swift
+++ TippingPot/SettingsViewController.swift
@@ -15,8 +15,8 @@ class SettingsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
         // Do any additional setup after loading the view.
+        loadDefaultTipValues()
         loadEditTipField()
     }
 
@@ -29,9 +29,21 @@ class SettingsViewController: UIViewController {
         // change the tip value of the selected segment
         // we assign to a new variable to prevent non-integer values
         // we also use bridgeToObjectiveC().integerValue because it would otherwise
-        // create a text value of "Optional(x)"
+        // create a text value of "Optional(x)" and using ! causes a fatal error
+        // when invalid values are pasted in
         var newTipValue = editTipField.text._bridgeToObjectiveC().integerValue
         tipControl.setTitle("\(newTipValue)%", forSegmentAtIndex: tipControl.selectedSegmentIndex)
+
+        //save to user defaults
+        //TODO(Andrew): clean this up, not familiar enough with swift enums
+        var defaults = NSUserDefaults.standardUserDefaults()
+        if (tipControl.selectedSegmentIndex == 0) {
+            defaults.setInteger(newTipValue, forKey: ViewController.TipOptions.FrugalTip.toRaw())
+        } else if (tipControl.selectedSegmentIndex == 1) {
+            defaults.setInteger(newTipValue, forKey: ViewController.TipOptions.RegularTip.toRaw())
+        } else if (tipControl.selectedSegmentIndex == 2) {
+            defaults.setInteger(newTipValue, forKey: ViewController.TipOptions.GenerousTip.toRaw())
+        }
     }
 
     @IBAction func onTipSelected(sender: AnyObject) {
@@ -42,6 +54,16 @@ class SettingsViewController: UIViewController {
         var selectedTipText = tipControl.titleForSegmentAtIndex(tipControl.selectedSegmentIndex)
         // Strip the %
         editTipField.text = selectedTipText.stringByReplacingOccurrencesOfString("%", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+    }
+    
+    // TODO(Andrew) this should really be a public static method
+    func loadDefaultTipValues() {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var tipValues = Array(Zip2([0,1,2], ViewController.TipOptions.allValues))
+        for (index, tipKey) in tipValues {
+            var tipValue = defaults.stringForKey(tipKey.toRaw())
+            tipControl.setTitle("\(tipValue!)%", forSegmentAtIndex: index)
+        }
     }
 
     @IBAction func onTap(sender: AnyObject) {
--- TippingPot/ViewController.swift
+++ TippingPot/ViewController.swift
@@ -13,14 +13,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
+    
+    enum TipOptions : String {
+        case FrugalTip = "frugal", RegularTip = "regular", GenerousTip = "generous"
+        
+        static let allValues = [FrugalTip, RegularTip, GenerousTip]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+
+        //check if we initialized already
+        var defaults = NSUserDefaults.standardUserDefaults()
+        if (defaults.objectForKey(TipOptions.FrugalTip.toRaw()) == nil) {
+            initializeDefaultTipValues()
+        }
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        loadDefaultTipValues()
+        calculateTotal()
     }
 
+    func initializeDefaultTipValues() {
+        var tipPercentageDefaults = [15, 20, 22]
+        var tipDefaults = Array(Zip2(tipPercentageDefaults, TipOptions.allValues))
+
+        var defaults = NSUserDefaults.standardUserDefaults()
+        for (value, key) in tipDefaults {
+            defaults.setInteger(value, forKey: key.toRaw())
+        }
+        defaults.synchronize()
+    }
+
+    func loadDefaultTipValues() {
+        var defaults = NSUserDefaults.standardUserDefaults()
+        var tipValues = Array(Zip2([0,1,2], TipOptions.allValues))
+        for (index, tipKey) in tipValues {
+            var tipValue = defaults.stringForKey(tipKey.toRaw())
+            tipControl.setTitle("\(tipValue!)%", forSegmentAtIndex: index)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -28,14 +65,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.15, 0.2, 0.22]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        calculateTotal()
+    }
+
+    func calculateTotal() {
+        var tipPercentage = getTipPercentage()
         var billAmount = billField.text._bridgeToObjectiveC().doubleValue
         var tip = billAmount * tipPercentage
         var total = billAmount + tip
 
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)
+    }
+
+    // Helper to convert the select segment into a tip value
+    func getTipPercentage() -> Double{
+        var tipString = tipControl.titleForSegmentAtIndex(tipControl.selectedSegmentIndex)
+        tipString = tipString.stringByReplacingOccurrencesOfString("%", withString: "", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        return tipString._bridgeToObjectiveC().doubleValue/100
     }
 
     @IBAction func onTap(sender: AnyObject) {
